1b3b4d877cc67a3eca33da53a9ecf6ea2e4a13d5
Santiago <98556731+S4nti4g0000@users.noreply.github.com>
2025-10-09 18:04:21 -0500

    Create button logic for the numbers
    By using similar code to the one in the Etch-a-sketch project, all the
    buttons inside a specific div are selected and stored inside a const
    value. Then each one of the buttons can have specific actions by
    checking the text inside it.
    
    This will be extended to operands as well.

--- script.js
+++ script.js
@@ -2,6 +2,46 @@ let valueA
 let valueB
 let operatorInput
 
+const numDisplay = document.getElementsByClassName("display")
+const numbers = document.getElementById("num-container").querySelectorAll("button")
+
+
+numbers.forEach(button => {
+    button.addEventListener("click", (ev) => {
+        let tar = ev.target
+        let content = tar.textContent.trim()
+
+        switch(content){
+            case '0':
+                console.log(0)
+                break;
+            case '1':
+                console.log(1)                
+                break;
+            case '2':                
+                break;
+            case '3':                
+                break;
+            case '4':                
+                break;
+            case '5':                
+                break;
+            case '6':                
+                break;
+            case '7':
+                console.log(7)               
+                break;
+            case '8':                
+                break;
+            case '9':                
+                break;
+            case 'AC': 
+                console.log('Clear!')               
+                break;
+        }
+    })
+})
+
 const sum = (...args) => {
     return args.reduce((acc, value) => acc + value, 0)
 }
