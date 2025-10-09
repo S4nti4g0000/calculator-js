9bb3164842096222a142f266efd5a56f8e150111
Santiago <98556731+S4nti4g0000@users.noreply.github.com>
2025-10-09 18:51:04 -0500

    Add logic to make operations
    A boolean "isSecondValue" determines if we're storing the first or the
    second value, this is initiated as false when the code runs as we're
    always starting with the first one.
    
    Since having one number stored is necessary to operate, the boolean is
    not changed unless an operator is selected, in that case, we can
    select the second number.
    
    Once the = button is pressed the bool will be set to false and the
    function to make the operation will be called, then the result is
    consoled log for testing.

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ let valueA
 let valueB
 let operatorInput
 
+let isSecondValue = false
+
 const numDisplay = document.getElementsByClassName("display")
 const numbers = document.getElementById("num-container").querySelectorAll("button")
+const operands = document.getElementById("op-container").querySelectorAll("button")
 
 
 numbers.forEach(button => {
@@ -13,30 +16,67 @@ numbers.forEach(button => {
 
         switch(content){
             case '0':
-                console.log(0)
+                (isSecondValue) ? valueB = 0:valueA = 0
                 break;
             case '1':
-                console.log(1)                
+                (isSecondValue) ? valueB = 1:valueA = 1               
                 break;
-            case '2':                
+            case '2':
+                (isSecondValue) ? valueB = 2:valueA = 2 
                 break;
-            case '3':                
+            case '3':    
+                (isSecondValue) ? valueB = 3:valueA = 3              
                 break;
-            case '4':                
+            case '4':  
+                (isSecondValue) ? valueB = 4:valueA = 4                
                 break;
-            case '5':                
+            case '5':
+                (isSecondValue) ? valueB = 5:valueA = 5                  
                 break;
-            case '6':                
+            case '6':
+                (isSecondValue) ? valueB = 6:valueA = 6                  
                 break;
             case '7':
-                console.log(7)               
+                (isSecondValue) ? valueB = 7:valueA = 7               
                 break;
-            case '8':                
+            case '8':
+                (isSecondValue) ? valueB = 8:valueA = 8                  
                 break;
-            case '9':                
+            case '9':
+                (isSecondValue) ? valueB = 9:valueA = 9                  
                 break;
             case 'AC': 
                 console.log('Clear!')               
+                break;
+        }
+    })
+})
+
+operands.forEach(button => {
+    button.addEventListener("click", (ev) =>{
+        let tar = ev.target
+        let content = tar.textContent.trim()
+
+        switch(content){
+            case '+':
+                isSecondValue = true
+                operatorInput = "+"
+                break;
+            case '-':
+                isSecondValue = true
+                operatorInput = "-"
+                break;
+            case '/':
+                isSecondValue = true
+                operatorInput = "/"
+                break;
+            case '*':
+                isSecondValue = true
+                operatorInput = "*"
+                break;
+            case '=':
+                isSecondValue = false
+                operate(valueA,valueB,operatorInput)
                 break;
         }
     })
@@ -60,12 +100,12 @@ const divide = (...args) => {
 
 function operate(valA, valB, operator){
     if(operator === '+'){
-        sum(valA,valB)
+        console.log(sum(valA,valB))
     }else if (operator === '-'){
-        subtract(valA,valB)
+        console.log(subtract(valA,valB))
     }else if (operator === '*'){
-        multiply(valA,valB)
+        console.log(multiply(valA,valB))
     }else if (operator === '/'){
-        divide(valA,valB)
+        console.log(divide(valA,valB))
     }
 }
