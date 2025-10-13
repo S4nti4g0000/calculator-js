ffacd30412bc303090b2bb305390e2ad3789a5b4
Santiago <98556731+S4nti4g0000@users.noreply.github.com>
2025-10-13 18:41:47 -0500

    Add logic to modify elements in DOM
    By selecting the ID's of each element the operand, and the numbers
    are displayed in each of the newly created divs.
    
    AC provisional code was also created.

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let operatorInput
 
 let isSecondValue = false
 
-const numDisplay = document.getElementsByClassName("display")
+const numDisplay = document.getElementById("display")
 const numbers = document.getElementById("num-container").querySelectorAll("button")
 const operands = document.getElementById("op-container").querySelectorAll("button")
 
@@ -13,41 +13,49 @@ numbers.forEach(button => {
     button.addEventListener("click", (ev) => {
         let tar = ev.target
         let content = tar.textContent.trim()
-
+        
         switch(content){
-            case '0':
-                (isSecondValue) ? valueB = 0:valueA = 0
+            case '0': 
+                isSecondValue ? valueB = 0 : valueA = 0
                 break;
             case '1':
-                (isSecondValue) ? valueB = 1:valueA = 1               
+                isSecondValue ? valueB = 1 : valueA = 1      
                 break;
             case '2':
-                (isSecondValue) ? valueB = 2:valueA = 2 
+                isSecondValue ? valueB = 2 : valueA = 2 
                 break;
             case '3':    
-                (isSecondValue) ? valueB = 3:valueA = 3              
+                isSecondValue ? valueB = 3 : valueA = 3            
                 break;
             case '4':  
-                (isSecondValue) ? valueB = 4:valueA = 4                
+                isSecondValue ? valueB = 4 : valueA = 4
                 break;
             case '5':
-                (isSecondValue) ? valueB = 5:valueA = 5                  
+                isSecondValue ? valueB = 5 : valueA = 5
                 break;
             case '6':
-                (isSecondValue) ? valueB = 6:valueA = 6                  
+                isSecondValue ? valueB = 6 : valueA = 6
                 break;
             case '7':
-                (isSecondValue) ? valueB = 7:valueA = 7               
+                isSecondValue ? valueB = 7 : valueA = 7
                 break;
             case '8':
-                (isSecondValue) ? valueB = 8:valueA = 8                  
+                isSecondValue ? valueB = 8 : valueA = 8     
                 break;
             case '9':
-                (isSecondValue) ? valueB = 9:valueA = 9                  
+                isSecondValue ? valueB = 9 : valueA = 9
                 break;
             case 'AC': 
-                console.log('Clear!')               
+                valueA = 0
+                valueB = 0 
+                operatorInput = ""              
                 break;
+        }
+
+        if(isSecondValue){
+            document.getElementById("numTwo").textContent = `${valueB}`
+        }else {
+            document.getElementById("numOne").textContent = `${valueA}`
         }
     })
 })
@@ -79,6 +87,7 @@ operands.forEach(button => {
                 operate(valueA,valueB,operatorInput)
                 break;
         }
+        if(isSecondValue) document.getElementById("operand").textContent = operatorInput
     })
 })
 
@@ -100,12 +109,12 @@ const divide = (...args) => {
 
 function operate(valA, valB, operator){
     if(operator === '+'){
-        console.log(sum(valA,valB))
+        document.getElementById("result").textContent = sum(valA,valB)
     }else if (operator === '-'){
-        console.log(subtract(valA,valB))
+        document.getElementById("result").textContent = subtract(valA,valB)
     }else if (operator === '*'){
-        console.log(multiply(valA,valB))
+        document.getElementById("result").textContent = multiply(valA,valB)
     }else if (operator === '/'){
-        console.log(divide(valA,valB))
+        document.getElementById("result").textContent = divide(valA,valB)
     }
 }
